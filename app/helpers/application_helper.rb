9f41bb6ec3aded63b5e9dcce61c29f3c4f800a8b
Sait Mesutcan Ilhaner <silhaner@oversee.net>
2014-04-26 14:49:44 -0700

    change to trigger the build

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Sait Mesutcan Ilhaner Sample App"
+    base_title = "Sait Ilhaner Sample App"
     if page_title.empty?
       base_title
     else
